a86bcbce15396e179918e09fb87528b7dfe5dfa0
codingpratice <codedotart.jsr@gmail.com>
2025-01-25 19:34:38 +0530

    first commit

---
+++ src/components/temp.js
@@ -0,0 +1,13 @@
+export default function Temp() {
+    return (
+      <div className="flex-container">
+        <div className="card">
+          <div className="card__header">Welcome</div>
+          <div className="card__body">
+            Learn more about SCSS and Tailwind!
+            <a href="https://tailwindcss.com"> Tailwind Docs</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
